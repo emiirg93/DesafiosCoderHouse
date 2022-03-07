471d0be69eb1e32129077eaf66d9739d87b77c6b
Emiliano Rago <emiliano.rg93@gmail.com>
2022-03-07 19:05:11 -0300

    estilado de codigo

--- ServidorRagoEmiliano/index.js
+++ ServidorRagoEmiliano/index.js
@@ -2,23 +2,25 @@ const http = require('http');
 
 const PORT = 3000;
 
-const server = http.createServer((req,res)=> {
+const server = http.createServer((req, res) => {
     res.end(JSON.stringify(createData()));
 });
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`Servidor Http levantado en el puerto ${PORT}`);
 })
 
 const createData = () => {
     return {
-        id: createRandomNumber(1,10),
-        title : `Producto ${createRandomNumber(1,10)}`,
-        price : createRandomNumber(0.00,9999.99,2),
-        thumbnail: `Foto ${createRandomNumber(1,10)}`
+        id: createRandomNumber(1, 10),
+        title: `Producto ${createRandomNumber(1, 10)}`,
+        price: createRandomNumber(0.00, 9999.99, 2),
+        thumbnail: `Foto ${createRandomNumber(1, 10)}`
     }
 }
 
-const createRandomNumber = (min,max,decimales) => {
-    return decimales? (Math.random() * (max - min) + min).toFixed(decimales) : Math.round(Math.random() * (max - min) + min);
+const createRandomNumber = (min, max, decimales) => {
+    return decimales ?
+        (Math.random() * (max - min) + min).toFixed(decimales) :
+        Math.round(Math.random() * (max - min) + min);
 }
